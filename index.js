e38b70fca6cb7c15bb5a4047041a2573b8e65e21
hashemsaadallah <hashemhamdy19@hotmail.com>
2023-03-26 04:53:41 +0200

    adding overlay for popup and fixing som spelling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function(){
     setTimeout(
         function open(event){
             document.querySelector(".popup").style.display = "block";
+            document.querySelector("#overlay").style.display = "block";
         },
         2000 
     )
@@ -10,7 +11,14 @@ window.addEventListener("load", function(){
 
 //programming close button on the popup
 document.querySelector("#popup-close").addEventListener("click", function(){
+    document.querySelector(".popup").style.animation = " fadeIn 2.5s"
+    setTimeout(
+        function close(event){
     document.querySelector(".popup").style.display = "none";
+    document.querySelector("#overlay").style.display = "none";
+        },
+        2000
+    )
 });
 
 
